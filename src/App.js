9d4fc270030b4a9fe6acc4ca4edbe0252cee0b6d
Juan Camilo <juanccamachob94@gmail.com>
2020-10-20 10:02:19 -0500

    Surprise first approach

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+import React, { useState, useEffect, Suspense } from 'react';
+// import logo from '/.logo.svg';
 import './App.css';
 
+const Surprise = React.lazy( () => import('./Surprise') );
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [surpriseShowable, setSurpriseShowable] = useState(false);
+
+  return(
+    <div>
+      <button onClick={ (env) => setSurpriseShowable(true) }>Mostrar sorpresa</button>
+      {
+        surpriseShowable && <Suspense><Surprise /></Suspense>
+      }
     </div>
   );
 }
